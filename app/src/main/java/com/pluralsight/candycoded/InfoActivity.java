89c4b507aa2ce3c190a7cad298b1733cbae12087
Samuel Ackon <ackons433@gmail.com>
2021-11-24 03:33:00 -0100

    hey

---
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -0,0 +1,49 @@
+package com.pluralsight.candycoded;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
+import android.net.Uri;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
+        ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
+        Picasso.with(this).
+                load(uri).
+                into(candyStoreImageView);
+
+
+    }
+
+    public void createMapIntent(View view){
+        Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801"));
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if(mapIntent.setPackage("com.google.android.apps.maps")!=null){
+            mapIntent.resolveActivity(getPackageManager());
+            startActivity(mapIntent);
+        }
+    }
+
+
+    // ***
+    // TODO - Task 3 - Launch the Phone Activity
+    // ***
+
+}
